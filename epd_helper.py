e3bcf6153e850dcd02a156085194e41db860be32
Nickxmodz <migsnonsport@gmail.com>
2024-12-17 21:57:41 -0500

    Simplify EPDHelper initialization by removing headless mode check and directly assigning epd_type

--- epd_helper.py
+++ epd_helper.py
@@ -8,11 +8,8 @@ logger = logging.getLogger(__name__)
 class EPDHelper:
     def __init__(self, epd_type):
         
-        if (self.epd_type  == "headless"):
-            self.epd_type = "epd2in7"
-        else:
-            self.epd_type = epd_type
-            
+        self.epd_type = epd_type
+
         self.epd = self._load_epd_module()
 
     def _load_epd_module(self):
